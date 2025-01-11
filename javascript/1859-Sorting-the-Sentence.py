86e243fd0001cb048a738d820f33d204f72def5c
Harishgouda G Patil <harishgouda213@gmail.com>
2025-01-11 04:55:27 +0000

    [1859] [Time Beats: 7.17%] [Memory Beats: 9.42%] - LeetPush

--- javascript/1859-Sorting-the-Sentence.py
+++ javascript/1859-Sorting-the-Sentence.py
@@ -1,6 +1,5 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        s=s.split()
-        sorted_words=sorted(s,key=lambda word:word[-1])
-        return " ".join([''.join([char for char in word if not char.isdigit()])for word in sorted_words])
-            
+       s=s.split()
+       sorted_words=sorted(s,key=lambda word:word[-1])
+       return " ".join([''.join([char for char in word if not char.isdigit()]) for word in sorted_words])
