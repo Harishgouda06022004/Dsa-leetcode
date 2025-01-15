d8e6192bd0d46012819c2470cc63a831dc5208d7
Harishgouda G Patil <harishgouda213@gmail.com>
2025-01-15 01:57:56 +0000

    [49] [Time Beats: 89.61%] [Memory Beats: 67.38%] - LeetPush

--- javascript/49-Group-Anagrams.py
+++ javascript/49-Group-Anagrams.py
@@ -8,7 +8,7 @@ class Solution:
             if s not in dict1:
                 dict1[s]=[]
             dict1[s].append(i)
-            result=[]
+            
         result=list(dict1.values())
 
         return result
