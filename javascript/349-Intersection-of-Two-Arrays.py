4baa573e8a600458ed981169c25f2413499e8c09
Harishgouda G Patil <harishgouda213@gmail.com>
2025-01-18 15:14:46 +0000

    [349] [Time Beats: 100.00%] [Memory Beats: 49.65%] - LeetPush

---
+++ javascript/349-Intersection-of-Two-Arrays.py
@@ -0,0 +1,11 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1=set(nums1)
+        print(list(nums1))
+        nums2=set(nums2)
+        print(list(nums2))
+        result=[]
+        for num in nums1:
+            if num in nums2:
+                result.append(num)
+        return result
