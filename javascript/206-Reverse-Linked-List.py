d1846f975f88fe34e46df6c9c965b13caaa8bae6
Harishgouda G Patil <harishgouda213@gmail.com>
2025-01-31 14:49:22 +0000

    [206] [Time Beats: 100.00%] [Memory Beats: 11.65%] - LeetPush

--- javascript/206-Reverse-Linked-List.py
+++ javascript/206-Reverse-Linked-List.py
@@ -5,11 +5,21 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev=None
-        current=head
-        while current:
-            next_node=current.next
-            current.next=prev
-            prev=current
-            current=next_node
-        return prev
+        # prev=None
+        # current=head
+        # while current:
+        #     next_node=current.next
+        #     current.next=prev
+        #     prev=current
+        #     current=next_node
+        # return prev
+        stack=[]
+        temp=head
+        while temp!=None:
+            stack.append(temp.val)
+            temp=temp.next
+        temp=head
+        while temp!=None:
+            temp.val=stack.pop()
+            temp=temp.next
+        return head
