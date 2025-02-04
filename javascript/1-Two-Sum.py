b60d3a4bab2182bda3c4e77711051ba55ecbc386
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-04 03:42:21 +0000

    [1] [Time Beats: 100.00%] [Memory Beats: 52.19%] - LeetPush

--- javascript/1-Two-Sum.py
+++ javascript/1-Two-Sum.py
@@ -1,6 +1,28 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            complement=target-nums[i]
-            if complement in nums[i+1:]:
-                return [i,nums.index(complement,i+1)]
+        # nums.sort()
+        # left=0
+        # right=len(nums)-1
+        # result=[]
+        # while left<right:
+        #     total=nums[left]+nums[right]
+        #     if total==target:
+        #         result.append(left)
+        #         result.append(right)
+        #         left+=1
+        #         right-=1
+        #     elif total<target:
+        #         left+=1
+        #     else:
+        #         right-=1
+        # # unique_list=[list(x) for x in set(tuple(sublist)) for sublist in result]
+        # # return unique_list
+        # return result
+        dict1={}
+        result=[]
+        for index,i in enumerate(nums):
+            complement=target-i
+            if complement in dict1:
+                return [index,dict1[complement]]
+            dict1[i]=index
+        return []
