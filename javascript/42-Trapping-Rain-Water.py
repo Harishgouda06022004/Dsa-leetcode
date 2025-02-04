ce9449db40418ec08dc936f3d7559b7c9cecc02b
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-04 18:31:25 +0000

    [42] [Time Beats: 22.65%] [Memory Beats: 8.11%] - LeetPush

--- javascript/42-Trapping-Rain-Water.py
+++ javascript/42-Trapping-Rain-Water.py
@@ -1,20 +1,39 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left=0
-        right=len(height)-1
-        left_max=height[left]
-        right_max=height[right]
-        water=0
-        while left<right:
-            if height[left]<height[right]:
+        # left=0
+        # right=len(height)-1
+        # left_max=height[left]
+        # right_max=height[right]
+        # water=0
+        # while left<right:
+        #     if height[left]<height[right]:
                 
-                left_max=max(left_max,height[left])
-                water+=max(0,left_max-height[left])
-                left+=1
-            else:
+        #         left_max=max(left_max,height[left])
+        #         water+=max(0,left_max-height[left])
+        #         left+=1
+        #     else:
                
-                right_max=max(right_max,height[right])
-                water+=max(0,right_max-height[right])
-                right-=1
-        return water
+        #         right_max=max(right_max,height[right])
+        #         water+=max(0,right_max-height[right])
+        #         right-=1
+        # return water
+        total=0
+        n=len(height)
+        prefix=[0]*n
+        prefix[0]=height[0]
+        for i in range(1,n-1):
+            prefix[i]=max(prefix[i-1],height[i])
+        print(prefix)
+        suffix=[0]*n
+        suffix[n-1]=height[n-1]
+        for i in range(n-2,-1,-1):
+            suffix[i]=max(suffix[i+1],height[i])
+        print(suffix)
+        
+        for i in range(1,n-1):
+            left_max=prefix[i]
+            right_max=suffix[i]
+            if height[i]<left_max and height[i]<right_max:
+                total+=min(left_max,right_max)-height[i]
+        return total
 
