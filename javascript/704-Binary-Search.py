7a4ca3c822ef9223e5a8c7e182a42bb01a1ee323
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-07 03:14:37 +0000

    [704] [Time Beats: 100.00%] [Memory Beats: 6.01%] - LeetPush

--- javascript/704-Binary-Search.py
+++ javascript/704-Binary-Search.py
@@ -1,17 +1,29 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        low=0
+        # low=0
+        # n=len(nums)
+        # high=n-1
+        # while low<=high:
+        #     mid=(low+high)//2
+        #     if target>nums[mid]:
+        #         low=mid+1
+        #     elif nums[mid]>target:
+        #         high=mid-1
+        #     elif nums[mid]==target:
+        #         return mid
+        #     else:
+        #        return mid
+        # return -1
         n=len(nums)
+        low=0
         high=n-1
         while low<=high:
             mid=(low+high)//2
-            if target>nums[mid]:
-                low=mid+1
-            elif nums[mid]>target:
-                high=mid-1
-            elif nums[mid]==target:
+            if nums[mid]==target:
                 return mid
+            elif nums[mid]<target:
+                low=mid+1
             else:
-               return mid
+                high=mid-1
         return -1
         
