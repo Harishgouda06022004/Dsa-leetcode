aa5976eae3c39ea642153f0e4fdd9e2e023f1c7b
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-09 05:15:52 +0000

    [217] [Time Beats: 5.17%] [Memory Beats: 17.10%] - LeetPush

--- javascript/217-Contains-Duplicate.py
+++ javascript/217-Contains-Duplicate.py
@@ -1,9 +1,20 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
+        # dict1={}
+        # for i in nums:
+        #     dict1[i]=dict1.get(i,0)+1
+        # for value in dict1.values():
+        #     if value>=2:
+        #         return True
+        # return False
         dict1={}
         for i in nums:
-            dict1[i]=dict1.get(i,0)+1
+            if i in dict1:
+                dict1[i]+=1
+            else:
+                dict1[i]=1
+        print(dict1)
         for value in dict1.values():
             if value>=2:
                 return True
