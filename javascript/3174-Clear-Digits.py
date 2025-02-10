298d42402dbf8f1223146959331e2e8d2b91b85d
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-10 08:01:37 +0000

    [3174] [Time Beats: 100.00%] [Memory Beats: 87.42%] - LeetPush

--- javascript/3174-Clear-Digits.py
+++ javascript/3174-Clear-Digits.py
@@ -1,17 +1,23 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        i = 0
-        while i < len(s):  # Iterate over the string
-            if s[i].isdigit():  # If it's a digit
-                if i > 0 and not s[i - 1].isdigit():  # If there's a non-digit to the left
-                    s = s[:i - 1] + s[i + 1:]  # Remove both the digit and the non-digit to its left
-                    i = 0  # Restart iteration since the string has changed
-                else:
-                    s = s[:i] + s[i + 1:]  # Remove the digit and keep the rest of the string
-                    i = 0  # Restart iteration since the string has changed
+        # i = 0
+        # while i < len(s):  # Iterate over the string
+        #     if s[i].isdigit():  # If it's a digit
+        #         if i > 0 and not s[i - 1].isdigit():  # If there's a non-digit to the left
+        #             s = s[:i - 1] + s[i + 1:]  # Remove both the digit and the non-digit to its left
+        #             i = 0  # Restart iteration since the string has changed
+        #         else:
+        #             s = s[:i] + s[i + 1:]  # Remove the digit and keep the rest of the string
+        #             i = 0  # Restart iteration since the string has changed
+        #     else:
+        #         i += 1  # Move to the next character
+        # return s
+        stack=[]
+        for char in s:
+            # while stack and stack[-1]==isdigit():
+            #     stack.pop()
+            if char.isdigit():
+                stack.pop()
             else:
-                i += 1  # Move to the next character
-        return s
-
-
-
+                stack.append(char)
+        return ''.join(stack)
