7d2bbb6b7d5e5c90ac6d67557830352bb0dd8690
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-11 13:33:16 +0000

    [540] [Time Beats: 5.36%] [Memory Beats: 5.09%] - LeetPush

---
+++ javascript/540-Single-Element-in-a-Sorted-Array.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        dict1={}
+        count=Counter(nums)
+        print(count)
+        return next(key for key,value in count.items() if value==1)
+        
+        
