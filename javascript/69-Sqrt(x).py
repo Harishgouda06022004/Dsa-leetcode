99e92cea1697ce939bb56e907cbf5105662ceac2
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-12 04:53:23 +0000

    [69] [Time Beats: 61.15%] [Memory Beats: 91.32%] - LeetPush

--- javascript/69-Sqrt(x).py
+++ javascript/69-Sqrt(x).py
@@ -1,3 +1,14 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(x**0.5)
+        low=1
+        high=x
+        while low<=high:
+            mid=(low+high)//2
+            Mid_squared=mid*mid
+            if Mid_squared==x:
+                return mid
+            elif Mid_squared<x:
+                low=mid+1
+            else:
+                high=mid-1
+        return high
