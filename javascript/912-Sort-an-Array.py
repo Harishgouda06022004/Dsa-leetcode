b3ff38b0ff9376581b2da4d474ea9f08ac4d6286
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-13 05:29:23 +0000

    [912] [Time Beats: 95.75%] [Memory Beats: 59.84%] - LeetPush

--- javascript/912-Sort-an-Array.py
+++ javascript/912-Sort-an-Array.py
@@ -1,4 +1,11 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
+        # if len(nums)<=1:
+        #     return nums
+        # pivot=nums[-1]
+        # print(pivot)
+        # left=[x for x in nums[:-1] if x<=pivot]
+        # right=[x for x in nums[:-1] if x>pivot]
+        # return self.sortArray(left)+[pivot]+self.sortArray(right)
         s=sorted(nums)
         return s
