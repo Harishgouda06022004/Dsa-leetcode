7ac5f89db7c0c5287d381bd57ad0db98a3c1c3fa
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-18 01:59:55 +0000

    [2375] [Time Beats: 25.88%] [Memory Beats: 83.23%] - LeetPush

---
+++ javascript/2375-Construct-Smallest-Number-From-DI-String.py
@@ -0,0 +1,29 @@
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        # num="1,2,3,4,5,6,7,8,9"
+
+        # n=len(pattern)
+        # for i in range(0,n+1):
+        #     if pattern[i]=='I':
+        counter=1
+        stack=[]
+        res=[]
+        n=len(pattern)
+        # for i in range(0,n):
+        #     stack.append(counter)
+        #     if pattern[i]=='D':
+        #         stack.append(counter+1)
+        #     if pattern[i]=='I':
+        #         res.append(str(stack.pop()))
+           
+        # return ''.join(res)
+        for char in pattern:
+            stack.append(counter)
+            counter+=1
+            if char=='I':
+                while stack:
+                    res.append(str(stack.pop()))
+        stack.append(counter)
+        while stack:
+            res.append(str(stack.pop()))
+        return ''.join(res)
