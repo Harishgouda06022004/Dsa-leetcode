351dccb760bbb083ea700c3d4f1dac58de141cc5
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-18 13:28:22 +0000

    [860] [Time Beats: 78.70%] [Memory Beats: 9.03%] - LeetPush

--- javascript/860-Lemonade-Change.py
+++ javascript/860-Lemonade-Change.py
@@ -6,9 +6,10 @@ class Solution:
             if b==5:
                 five+=1
             if b==10:
-                if five==0:
+                if five>0:
+                    five-=1
+                else:
                     return False
-                five-=1
                 ten+=1                   
             if b==20:
                 if five>0 and ten>0:
