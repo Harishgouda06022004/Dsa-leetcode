7f08e7a1eceaa76ab57f6588f017f20189d9977a
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-22 01:52:29 +0000

    [977] [Time Beats: 60.80%] [Memory Beats: 38.02%] - LeetPush

--- javascript/977-Squares-of-a-Sorted-Array.py
+++ javascript/977-Squares-of-a-Sorted-Array.py
@@ -1,8 +1,19 @@
 class Solution(object):
     def sortedSquares(self, nums):
-        num=[]
-        for i in range(len(nums)):
-            nums[i]=nums[i]*nums[i]
-            num.append(nums[i])
-        return sorted(num)
+        # num=[]
+        # for i in range(len(nums)):
+        #     nums[i]=nums[i]*nums[i]
+        #     num.append(nums[i])
+        # return sorted(num)
+        left=0
+        right=len(nums)-1
+        result=[]
+        while left<=right:
+            if abs(nums[left])>abs(nums[right]):
+                result.append(nums[left]**2)
+                left+=1
+            else:
+                result.append(nums[right]**2)
+                right-=1
+        return result[::-1]
         
