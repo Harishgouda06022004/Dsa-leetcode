062873db7eeb8b48cef1458a3de5d214119a291d
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-22 02:06:39 +0000

    [2351] [Time Beats: 100.00%] [Memory Beats: 50.56%] - LeetPush

---
+++ javascript/2351-First-Letter-to-Appear-Twice.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def repeatedCharacter(self, s):
+        dict1={}
+
+        for letter in s:
+            if letter in dict1:
+                dict1[letter]+=1
+                for key,val in dict1.items():
+                    if val==2:
+                        return key
+            else:
+                dict1[letter]=1
+        print(dict1)
+        
+        # return max(dict1,key=dict1.get)
+        
