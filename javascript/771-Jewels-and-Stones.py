852916a820462f5d0942884fabe36252d418d269
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-22 01:41:25 +0000

    [771] [Time Beats: 100.00%] [Memory Beats: 80.23%] - LeetPush

--- javascript/771-Jewels-and-Stones.py
+++ javascript/771-Jewels-and-Stones.py
@@ -1,15 +1,8 @@
-class Solution:
-    def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        dict1={}
-        dict2={}
-        for i in jewels:
-            dict1[i]=dict1.get(i,0)+1
-        print(dict1)
-        for i in stones:
-            dict2[i]=dict2.get(i,0)+1
-        print(dict2)
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
         count=0
-        for i in dict2:
-            if i in dict1:
-                count+=dict2[i]
+        for s in stones:
+            if s in jewels:
+                count+=1
         return count
+        
