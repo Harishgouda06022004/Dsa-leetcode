0628974755cd61fc6ddf04e2cbf0a7443d40e0f4
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-23 01:37:07 +0000

    [771] [Time Beats: 100.00%] [Memory Beats: 80.33%] - LeetPush

--- javascript/771-Jewels-and-Stones.py
+++ javascript/771-Jewels-and-Stones.py
@@ -1,8 +1,13 @@
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
+        # count=0
+        # for s in stones:
+        #     if s in jewels:
+        #         count+=1
+        # return count
         count=0
+        jewels=set(jewels)
         for s in stones:
             if s in jewels:
                 count+=1
         return count
-        
