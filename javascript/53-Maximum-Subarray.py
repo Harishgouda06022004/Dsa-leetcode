f0ed1e37109bdca1e35ffc0597f9659e4bca506d
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-23 01:04:42 +0000

    [53] [Time Beats: 66.91%] [Memory Beats: 85.88%] - LeetPush

--- javascript/53-Maximum-Subarray.py
+++ javascript/53-Maximum-Subarray.py
@@ -1,18 +1,12 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        # n=len(nums)
-        # max_sum=float('-inf')
-        # for i in range(0,n):
-        #     current_sum=0
-        #     for j in range(i,n):
-        #         current_sum+=nums[j]
-        #         max_sum=max(current_sum,max_sum)
-        # return max_sum
-        max_sum=nums[0]
+class Solution(object):
+    def maxSubArray(self, nums):
+        n=len(nums)
         curr_sum=0
-        for n in nums:
+        max_sum=float('-inf')
+        for i in range(n):
+            curr_sum+=nums[i]
+            max_sum=max(max_sum,curr_sum)
             if curr_sum<0:
                 curr_sum=0
-            curr_sum+=n
-            max_sum=max(max_sum,curr_sum)
         return max_sum
+        
