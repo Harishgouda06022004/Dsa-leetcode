6af423c6c9d39280ed9c420b172d087b0794f1d3
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-23 00:46:49 +0000

    [503] [Time Beats: 5.66%] [Memory Beats: 49.25%] - LeetPush

--- javascript/503-Next-Greater-Element-II.py
+++ javascript/503-Next-Greater-Element-II.py
@@ -1,45 +1,12 @@
-class Solution:
-    def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        
-        # for num in nums:
-        #     print(nums)
-        #     nums_greater=-1
-        #     for j in nums:
-        #         if nums[j]>nums[num]:
-        #             nums_greater=nums[j]
-        #     result.append(nums_greater)
-        # return result
-        # n=len(nums)
-        
-        # for num in range(0,n):
-        #     nums_greater=-1
-        #     for j in range(num+1,n):
-        #         if nums[j]>nums[num]:
-        #             nums_greater=nums[j]
-        #             break
-        #     result.append(nums_greater)
-        # return result
-        # n = len(nums)
-        # result = [-1] * n  # Initialize result array with -1
-        # stack = []  # Monotonic decreasing stack (stores indices)
-
-        # for i in range(n):
-        #     while stack and nums[stack[-1]] < nums[i]:
-        #         index = stack.pop()  # Pop smaller elements
-        #         result[index] = nums[i]  # Update the result for popped index
-            
-        #     stack.append(i)  # Push current index onto stack
-
-        # return result
+class Solution(object):
+    def nextGreaterElements(self, nums):
         n=len(nums)
-        stack=[]
-        nge=[2]*n
-        for i in range(2*n-1,-1,-1):
-            while stack and stack[-1]<=nums[i%n]:
-                stack.pop()
-           
-            if i<n:
-                nge[i]=-1 if not stack else stack[-1]
-            stack.append(nums[i%n])
-        return nge
-        
+        greater=[]
+        for i in range(len(nums)):
+            next_greater=-1
+            for j in range(i+1,i+n):
+                if nums[j%n]>nums[i]:
+                    next_greater=nums[j%n]
+                    break
+            greater.append(next_greater)
+        return greater
