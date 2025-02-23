71c7cce81066b936314670d9b95d8ab75788cb73
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-23 00:13:09 +0000

    [49] [Time Beats: 71.45%] [Memory Beats: 81.82%] - LeetPush

--- javascript/49-Group-Anagrams.py
+++ javascript/49-Group-Anagrams.py
@@ -6,5 +6,15 @@ class Solution(object):
             if c not in dict1:
                 dict1[c]=[]
             dict1[c].append(s)
-        result=list(dict1.values())
-        return result
+        # print(dict1)
+        # result=list(dict1.values())
+        return dict1.values()
+        # dict1={}
+        # for s in strs:
+        #     s_sorted=sorted(s)
+        #     key=tuple(s_sorted) #keeping it in tuple form form because key are always immutable
+        #     if key not in dict1:
+        #         dict1[key]=[s]
+        #     else:
+        #         dict1[key].append(s)
+        # return dict1.values()
