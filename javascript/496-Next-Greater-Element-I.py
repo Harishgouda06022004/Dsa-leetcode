1bb7724d92993047f1ea430b9173df72791c2442
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-23 00:39:08 +0000

    [496] [Time Beats: 24.37%] [Memory Beats: 53.76%] - LeetPush

--- javascript/496-Next-Greater-Element-I.py
+++ javascript/496-Next-Greater-Element-I.py
@@ -1,12 +1,12 @@
-class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        result=[]
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        greater=[]
         for num in nums1:
+            next_greater=-1
             index=nums2.index(num)
-            nums_greater=-1
             for j in range(index+1,len(nums2)):
                 if nums2[j]>num:
-                    nums_greater=nums2[j]
+                    next_greater=nums2[j]
                     break
-            result.append(nums_greater)
-        return result
+            greater.append(next_greater)
+        return greater
