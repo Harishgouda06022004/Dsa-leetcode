d59aa0bbdc7b753d3472ff569fd447ca1465f913
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-23 01:42:06 +0000

    [503] [Time Beats: 94.15%] [Memory Beats: 67.17%] - LeetPush

--- javascript/503-Next-Greater-Element-II.py
+++ javascript/503-Next-Greater-Element-II.py
@@ -1,12 +1,22 @@
 class Solution(object):
     def nextGreaterElements(self, nums):
+        # n=len(nums)
+        # greater=[]
+        # for i in range(len(nums)):
+        #     next_greater=-1
+        #     for j in range(i+1,i+n):
+        #         if nums[j%n]>nums[i]:
+        #             next_greater=nums[j%n]
+        #             break
+        #     greater.append(next_greater)
+        # return greater
         n=len(nums)
-        greater=[]
-        for i in range(len(nums)):
-            next_greater=-1
-            for j in range(i+1,i+n):
-                if nums[j%n]>nums[i]:
-                    next_greater=nums[j%n]
-                    break
-            greater.append(next_greater)
-        return greater
+        result=[-1]*n
+        stack=[]
+        for i in range(2*n):
+            while stack and nums[i%n]>nums[stack[-1]]:
+                index=stack.pop()
+                result[index]=nums[i%n]
+            if i<n:
+                stack.append(i)
+        return result
