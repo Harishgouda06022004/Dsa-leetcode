9c25c03ff0c133752f0a23729060268ec0670ba0
Harishgouda G Patil <harishgouda213@gmail.com>
2025-02-27 05:40:08 +0000

    [873] [Time Beats: 38.10%] [Memory Beats: 90.48%] - LeetPush

--- javascript/873-Length-of-Longest-Fibonacci-Subsequence.py
+++ javascript/873-Length-of-Longest-Fibonacci-Subsequence.py
@@ -2,13 +2,13 @@ class Solution(object):
     def lenLongestFibSubseq(self, arr):
         n=len(arr)
         arr_set=set(arr)
-        max_length=0
+        max_lenght=0
         for i in range(0,n):
             for j in range(i+1,n):
                 x,y=arr[i],arr[j]
-                length=2
+                lenght=2
                 while x+y in arr_set:
                     x,y=y,x+y
-                    length+=1
-                max_length=max(max_length,length)
-        return max_length if max_length>=3 else 0
+                    lenght+=1
+                max_lenght=max(max_lenght,lenght)
+        return max_lenght if max_lenght>=3 else 0
