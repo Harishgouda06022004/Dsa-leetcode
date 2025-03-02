5686dbde4c85dc38855541c0364f4d06984af929
Harishgouda G Patil <harishgouda213@gmail.com>
2025-03-02 04:47:35 +0000

    [2798] [Time Beats: 100.00%] [Memory Beats: 34.51%] - LeetPush

--- javascript/Number-of-Employees-Who-Met-the-Target.py
+++ javascript/Number-of-Employees-Who-Met-the-Target.py
@@ -1,7 +1,8 @@
 class Solution:
     def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
-        count=0
-        for num in hours:
-            if num>=target:
-                count+=1
-        return count
+        # count=0
+        # for num in hours:
+        #     if num>=target:
+        #         count+=1
+        # return count
+        return sum(num>=target for num in hours)
