d2eea40916474b50af9a1001e27b1ba3ce7d25d6
Harishgouda G Patil <harishgouda213@gmail.com>
2025-03-03 03:16:17 +0000

    [26] [Time Beats: 48.45%] [Memory Beats: 49.81%] - LeetPush

--- javascript/Remove-Duplicates-from-Sorted-Array.py
+++ javascript/Remove-Duplicates-from-Sorted-Array.py
@@ -1,11 +1,20 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        seen=set()
-        index=0
-        for num in nums:
-            if num not in seen:
-                seen.add(num)
-                nums[index]=num
-                index+=1
-        return index
+        # seen=set()
+        # index=0
+        # for num in nums:
+        #     if num not in seen:
+        #         seen.add(num)
+        #         nums[index]=num
+        #         index+=1
+        # return index
+        n=len(nums)
+        i=1
+        j=1
+        for i in range(1,n):
+            if nums[i]!=nums[i-1]:
+                nums[j]=nums[i]
+                j+=1
+        return j
+
         
