471b19c7740b2fd30395aef6c873a68e3a0f1e5e
Harishgouda G Patil <harishgouda213@gmail.com>
2025-03-08 13:29:32 +0000

    [1512] [Time Beats: 100.00%] [Memory Beats: 31.30%] - LeetPush

--- javascript/Number-of-Good-Pairs.py
+++ javascript/Number-of-Good-Pairs.py
@@ -1,9 +1,16 @@
+from collections import Counter
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        dict1={}
+        # dict1={}
+        # count=0
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]==nums[j]:
+        #             count+=1
+        # return count
         count=0
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]==nums[j]:
-                    count+=1
+        dict1=Counter(nums)
+        print(dict1)
+        for val in dict1.values():
+            count+=val*(val-1)//2
         return count
