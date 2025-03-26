a2abcfa15e74b63750135e99de276a67a01fbe9b
Harishgouda G Patil <harishgouda213@gmail.com>
2025-03-26 01:51:57 +0000

    [453] [Time Beats: 51.17%] [Memory Beats: 92.60%] - LeetPush

--- javascript/Minimum-Moves-to-Equal-Array-Elements.py
+++ javascript/Minimum-Moves-to-Equal-Array-Elements.py
@@ -2,9 +2,12 @@ class Solution:
     def minMoves(self, nums: List[int]) -> int:
         min_nums=min(nums)
         moves=0
-        # return sum(num-min_nums for num in nums)
-        for num in nums:
-            diff=num-min_nums
-            moves+=diff
-            print(f\num: {num}, min_num: {min_nums}, num - min_num: {diff}, moves so far: {moves}\)
-        return moves
+        return sum(num-min_nums for num in nums)
+        # for num in nums:
+        #     diff=num-min_nums
+        #     moves+=diff
+        #     print(f\num: {num}, min_num: {min_nums}, num - min_num: {diff}, moves so far: {moves}\)
+        # return moves
+        moves=0
+        # if len(set(nums))==1:
+        #     return 
