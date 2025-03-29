8d6b877ad640eee20a4ad2c753f4b0b39622dfd0
Harishgouda G Patil <harishgouda213@gmail.com>
2025-03-29 15:12:37 +0000

    [1731] [Time Beats: 83.06%] - LeetPush

---
+++ javascript/The-Number-of-Employees-Which-Report-to-Each-Employee.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select e.employee_id,e.name,count(*) as reports_count,round(avg(b.age),0) as average_age
+from Employees e join employees b on e.employee_id=b.reports_to
+group by e.employee_id,e.name
+order by e.employee_id
