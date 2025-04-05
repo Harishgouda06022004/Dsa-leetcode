47c35d4ae14a5f7e1f2205ca28e75c67994260d0
Harishgouda G Patil <harishgouda213@gmail.com>
2025-04-05 09:00:58 +0000

    [1863] [Time Beats: 100.00%] [Memory Beats: 41.01%] - LeetPush

--- javascript/Sum-of-All-Subset-XOR-Totals.py
+++ javascript/Sum-of-All-Subset-XOR-Totals.py
@@ -1,11 +1,20 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        total=0
-        n=len(nums)
-        for mask in range(1<<n):
-            xor_total=0
-            for i in range(n):
-                if mask & (1<<i):
-                    xor_total^=nums[i]
-            total+=xor_total
-        return total
+        # total=0
+        # n=len(nums)
+        # for mask in range(1<<n):
+        #     xor_total=0
+        #     for i in range(n):
+        #         if mask & (1<<i):
+        #             xor_total^=nums[i]
+        #     total+=xor_total
+        # return total
+        # def dfs(i,total):
+        #     if i==len(nums):
+        #         return total
+        #     return dfs(i+1,total^nums[i])+dfs(i+1,total)
+        # return dfs(0,0)
+        res=0
+        for n in nums:
+            res |=n
+        return res*2**(len(nums)-1)
