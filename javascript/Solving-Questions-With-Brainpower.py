c11e9ff6b93fb44529f531efd58213964171af83
Harishgouda G Patil <harishgouda213@gmail.com>
2025-04-12 02:05:06 +0000

    [2140] [Time Beats: 28.02%] [Memory Beats: 28.68%] - LeetPush

--- javascript/Solving-Questions-With-Brainpower.py
+++ javascript/Solving-Questions-With-Brainpower.py
@@ -12,15 +12,23 @@ class Solution:
         #     max_points=max(max_points,points)
         # return max_points
         cache=[0]*len(questions)
+        # print(cache)
         def backtrack(i):
             if i>=len(questions):
+                # print(f"Reached end at i={i}, returning 0")
                 return 0
             if cache[i]:
+                # print(f"Using cached value at i={i} → {cache[i]}")
                 return cache[i]
             points,brainpower=questions[i]
-            cache[i]= max(
-                backtrack(i+1),
-                points+backtrack(i+1+brainpower)
-            )
+            # print(f"\nAt i={i}, Question: [points={points}, brainpower={brainpower}]")
+            # print(f"--> Calling backtrack({i + 1}) to skip")
+            # print(f"--> Calling backtrack({i + 1 + brainpower}) to solve and jump")
+            skip =backtrack(i+1)
+            solve=points+backtrack(i+1+brainpower)
+            cache[i]= max(skip,solve)
+            # print(cache)
+            # print(f"Computed cache[{i}] = {cache[i]}")
             return cache[i]
+           
         return backtrack(0)
