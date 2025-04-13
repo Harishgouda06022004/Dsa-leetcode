638c2540abb87170b8f611a5eca30ce2a01153dc
Harishgouda G Patil <harishgouda213@gmail.com>
2025-04-13 06:05:36 +0000

    [7] [Time Beats: 51.01%] [Memory Beats: 36.81%] - LeetPush

--- javascript/Reverse-Integer.py
+++ javascript/Reverse-Integer.py
@@ -1,27 +1,37 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x==1534236469:
-            return 0
-        if x==2147483647:
-            return 0
-        if x==-2147483648:
-            return 0
-        if x==1563847412:
-            return 0
-        if x==-1563847412:
-            return 0
+        # if x==1534236469:
+        #     return 0
+        # if x==2147483647:
+        #     return 0
+        # if x==-2147483648:
+        #     return 0
+        # if x==1563847412:
+        #     return 0
+        # if x==-1563847412:
+        #     return 0
         
-        if x ==1147483648:
-            return 0
-        if x ==1137464807:
-            return 0
-        if x ==1235466808:
-            return 0
-        if x ==1221567417:
-            return 0
+        # if x ==1147483648:
+        #     return 0
+        # if x ==1137464807:
+        #     return 0
+        # if x ==1235466808:
+        #     return 0
+        # if x ==1221567417:
+        #     return 0
+        # if x<0:
+        #     x=-x
+        #     reversed_x=int(str(x)[::-1])
+        #     return -reversed_x
+        # else:
+        #     return int(str(x)[::-1])
+        x_str=str(x)
         if x<0:
-            x=-x
-            reversed_x=int(str(x)[::-1])
-            return -reversed_x
+            x_str=x_str[1:]
+            x_str='-'+x_str[::-1]
         else:
-            return int(str(x)[::-1])
+            x_str=x_str[::-1]
+        res=int(x_str)
+        if ((res>(2**31-1)) or (res<-2**31)):
+            return 0
+        return res
