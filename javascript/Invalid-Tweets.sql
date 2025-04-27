24d739c7556fab40bdd58cbca6e6bf3edeb08705
Harishgouda G Patil <harishgouda213@gmail.com>
2025-04-27 15:28:35 +0000

    [1683] [Time Beats: 80.67%] - LeetPush

--- javascript/Invalid-Tweets.sql
+++ javascript/Invalid-Tweets.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
-SELECT tweet_id
-FROM Tweets
-WHERE LENGTH(content)>15
-ORDER BY tweet_id ASC
+-- SELECT tweet_id
+-- FROM Tweets
+-- WHERE LENGTH(content)>15
+-- ORDER BY tweet_id ASC
+select tweet_id
+from Tweets
+where length(content)>15
+order by tweet_id asc
