9d428668bf32a70e04a3f57897a8ca775abcf05d
Harishgouda G Patil <harishgouda213@gmail.com>
2025-05-08 07:34:07 +0530

    [1378] [Time Beats: 57.33%] - LeetPush

--- javascript/Replace-Employee-ID-With-The-Unique-Identifier.sql
+++ javascript/Replace-Employee-ID-With-The-Unique-Identifier.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
-SELECT euni.unique_id,e.name
-FROM Employees e
-LEFT JOIN EmployeeUNI euni
-ON e.id=euni.id;
+-- SELECT euni.unique_id,e.name
+-- FROM Employees e
+-- LEFT JOIN EmployeeUNI euni
+-- ON e.id=euni.id;
+select euni.unique_id,e.name
+from Employees e
+left join EmployeeUNI euni
+on e.id=euni.id
