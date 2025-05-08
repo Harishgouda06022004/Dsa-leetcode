6817e54d00f71eb3b4bea086354f3c21c926c148
Harishgouda G Patil <harishgouda213@gmail.com>
2025-05-08 22:57:26 +0530

    [55] [Time Beats: 40.69%] [Memory Beats: 43.08%] - LeetPush

--- javascript/Jump-Game.py
+++ javascript/Jump-Game.py
@@ -10,14 +10,20 @@ class Solution:
         # for num in nums:
         #     if num==0:
         #         return False
-        target=len(nums)-1
-        for i in range(len(nums)-2,-1,-1):
-            if i+nums[i]>=target:
-                target=i    
-        return target==0
+        # target=len(nums)-1
+        # for i in range(len(nums)-2,-1,-1):
+        #     if i+nums[i]>=target:
+        #         target=i    
+        # return target==0
         # target=len(nums)-1
         # for i in range(len(nums)-2,-1,-1):
         #     if i+nums[i]>=target:
         #         target=i
                 
         # return  target==0
+        max_reach=0
+        for i in range(len(nums)):
+            if i>max_reach:
+                return False
+            max_reach=max(max_reach,i+nums[i])
+        return True
