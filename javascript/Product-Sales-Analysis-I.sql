2569253ebdd3253f34dd5d474b3a0e44fe45ad58
Harishgouda G Patil <harishgouda213@gmail.com>
2025-05-08 07:52:40 +0530

    [1068] [Time Beats: 67.09%] - LeetPush

--- javascript/Product-Sales-Analysis-I.sql
+++ javascript/Product-Sales-Analysis-I.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
-SELECT p.product_name,s.year,s.price
-FROM Sales s
-JOIN Product p
-ON s.product_id=p.product_id
+-- SELECT p.product_name,s.year,s.price
+-- FROM Sales s
+-- JOIN Product p
+-- ON s.product_id=p.product_id
+select p.product_name,s.year,s.price
+from Sales s
+inner join Product p
+on p.product_id=s.product_id
