4e38307864d0d3dc7295594294a949df3bdc3409
Harishgouda G Patil <harishgouda213@gmail.com>
2025-05-08 21:41:02 +0530

    [20] [Time Beats: 100.00%] [Memory Beats: 80.98%] - LeetPush

---
+++ javascript/Valid-Parentheses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        # n=len(s)
+        # print(n)
+        for i in s:
+            if i=='(' or i=='{' or i=='[':
+                stack.append(i)
+            else:
+                if len(stack)==0:
+                    return False
+                top=stack[-1]
+                
+                if i==')' and top==\(\ or i==\}\ and top==\{\ or i==\]\ and top==\[\:
+                    stack.pop()
+                else:
+                    return False
+        return len(stack)==0
+        stack=[]
+        for i in s:
+            if i=='(' or i=='{'or i=='[':
+                stack.append(i)
+            else:
+                if len(stack)==0:
+                    return False
+                top=stack[-1]
+                if i==')' and top=='(' or i=='}' and top=='{' or i==']' and top==']':
+                    stack.pop()
+                else:
+                    return False
+        return len(stack)==0
