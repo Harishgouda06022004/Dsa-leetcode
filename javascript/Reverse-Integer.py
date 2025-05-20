28c91e7888a69238b4c07c03dd705eaa0f455fe3
Harishgouda G Patil <harishgouda213@gmail.com>
2025-05-20 07:43:25 +0530

    [7] [Time Beats: 7.19%] [Memory Beats: 82.30%] - LeetPush

--- javascript/Reverse-Integer.py
+++ javascript/Reverse-Integer.py
@@ -26,12 +26,28 @@ class Solution:
         # else:
         #     return int(str(x)[::-1])
         x_str=str(x)
+        # x_str=x_str[1:]
+        # print(x_str)
         if x<0:
-            x_str=x_str[1:]
-            x_str='-'+x_str[::-1]
+            x_str=x_str[1:] #skip the minus sign we use x_str=x_str[1:] see like -123 which will become 123
+            print(x_str)
+            x_str='-'+x_str[::-1] # then we reattach it here
+        # print(x_str)
         else:
             x_str=x_str[::-1]
+        
         res=int(x_str)
         if ((res>(2**31-1)) or (res<-2**31)):
             return 0
         return res
+        # rev=0
+        # sign=-1 if x<0 else +1
+        # x=abs(x)
+        # while x!=0:
+        #     digit=x%10
+        #     rev=rev*10+digit
+        #     x=x//10
+        # rev*=sign
+        # if rev>(2**31-1) or rev<-2**31:
+        #     return 0
+        # return rev
