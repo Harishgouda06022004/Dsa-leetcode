26b299dd3a1c7225fded6ab2c99a09d130c46f2e
Harishgouda G Patil <harishgouda213@gmail.com>
2025-05-21 10:59:29 +0530

    [238] [Time Beats: 5.57%] [Memory Beats: 5.51%] - LeetPush

---
+++ javascript/Product-of-Array-Except-Self.py
@@ -0,0 +1,42 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # n=len(nums)
+        # prefix=[0]*n
+        # prefix[0]=1
+        # for i in range(1,n):
+        #     prefix[i]=prefix[i-1]*nums[i-1]
+        # print(prefix)
+        # suffix=[0]*n
+        # suffix[n-1]=1
+        # for i in range(n-2,-1,-1):
+        #     suffix[i]=suffix[i+1]*nums[i+1]
+        # print(suffix)
+        # answers=[0]*n
+        # for i in range(0,n):
+        #     answers[i]=prefix[i]*suffix[i]
+        # return answers
+        # result=[]
+        
+        # for i in range(n):
+        #     product=1
+        #     for j in range(n):
+        #         if i!=j:
+        #             product*=nums[j]
+        #     result.append(product)
+        # return result
+        n=len(nums)
+        prefix=[0]*n
+        prefix[0]=1
+        for i in range(1,n):
+            prefix[i]=prefix[i-1]*nums[i-1]
+        print(prefix)
+        suffix=[0]*n
+        suffix[n-1]=1
+        for i in range(n-2,-1,-1):
+            suffix[i]=suffix[i+1]*nums[i+1]
+        print(suffix)
+        answers=[0]*n
+        for i in range(0,n):
+            answers[i]=prefix[i]*suffix[i]
+        print(answers)
+        return answers
