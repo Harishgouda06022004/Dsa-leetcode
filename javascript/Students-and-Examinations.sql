1fbfd6bb968fde9a65eb1f45a7eac2ec1114134b
Harishgouda G Patil <harishgouda213@gmail.com>
2025-05-24 08:31:00 +0530

    [1280] [Time Beats: 97.52%] - LeetPush

--- javascript/Students-and-Examinations.sql
+++ javascript/Students-and-Examinations.sql
@@ -1,9 +1,17 @@
 # Write your MySQL query statement below
+-- select s.student_id,s.student_name,su.subject_name,count(e.student_id) attended_exams
+-- from Students s
+-- join Subjects su
+-- left join Examinations e
+--     on s.student_id=e.student_id
+--     and e.subject_name=su.subject_name
+-- group by s.student_id,s.student_name,su.subject_name
+-- order by s.student_id,s.student_name,su.subject_name;
 select s.student_id,s.student_name,su.subject_name,count(e.student_id) attended_exams
 from Students s
 join Subjects su
 left join Examinations e
-    on s.student_id=e.student_id
-    and e.subject_name=su.subject_name
+on s.student_id=e.student_id
+and e.subject_name=su.subject_name
 group by s.student_id,s.student_name,su.subject_name
 order by s.student_id,s.student_name,su.subject_name;
