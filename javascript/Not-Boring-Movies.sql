8170e4129523d9fee685b58657bb3f7615712710
Harishgouda G Patil <harishgouda213@gmail.com>
2025-05-24 08:37:46 +0530

    [620] [Time Beats: 72.20%] - LeetPush

--- javascript/Not-Boring-Movies.sql
+++ javascript/Not-Boring-Movies.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select c.id,c.movie,c.description,c.rating 
 from Cinema c
-where c.description!='boring' and c.id&1=1
+where c.description!='boring' and c.id%2!=0
 order by c.rating desc
