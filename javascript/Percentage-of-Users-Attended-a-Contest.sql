440d38009114d392fb119b34eec8ffe8412defcb
Harishgouda G Patil <harishgouda213@gmail.com>
2025-05-26 06:23:09 +0530

    [1633] [Time Beats: 99.36%] - LeetPush

--- javascript/Percentage-of-Users-Attended-a-Contest.sql
+++ javascript/Percentage-of-Users-Attended-a-Contest.sql
@@ -1,7 +1,13 @@
 # Write your MySQL query statement below
-select r.contest_id,round(count(distinct r.user_id)*100.0/(select count(*)from Users),2) as percentage
+-- select r.contest_id,round(count(distinct r.user_id)*100.0/(select count(*)from Users),2) as percentage
+-- from Users u
+-- join Register r
+-- on u.user_id=r.user_id
+-- group by r.contest_id
+-- order by percentage desc, r.contest_id asc
+select r.contest_id,round(count(distinct r.user_id)*100.0/(select count(*) from Users),2) as percentage
 from Users u
 join Register r
 on u.user_id=r.user_id
 group by r.contest_id
-order by percentage desc, r.contest_id asc
+order by percentage desc,r.contest_id asc
