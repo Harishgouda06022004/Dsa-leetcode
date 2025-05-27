76a8a06881a8dda218e8a727ca8438a5e3d3804d
Harishgouda G Patil <harishgouda213@gmail.com>
2025-05-27 06:05:06 +0530

    [1211] [Time Beats: 79.00%] - LeetPush

--- javascript/Queries-Quality-and-Percentage.sql
+++ javascript/Queries-Quality-and-Percentage.sql
@@ -1,7 +1,11 @@
 # Write your MySQL query statement below
-select 
-q.query_name,
-round(avg(rating*1.0/position),2)as quality,
+-- select 
+-- q.query_name,
+-- round(avg(rating*1.0/position),2)as quality,
+-- round(sum(case when rating<3 then 1 else 0 end)*100/count(*),2) as poor_query_percentage
+-- from Queries q
+-- group by q.query_name
+select q.query_name,round(avg(rating/position),2) as quality,
 round(sum(case when rating<3 then 1 else 0 end)*100/count(*),2) as poor_query_percentage
 from Queries q
 group by q.query_name
