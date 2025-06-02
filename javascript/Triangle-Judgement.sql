d27782234518ce1b9b6dd33debb92a7d9e25023d
Harishgouda G Patil <harishgouda213@gmail.com>
2025-06-02 06:09:13 +0530

    [610] [Time Beats: 66.60%] - LeetPush

--- javascript/Triangle-Judgement.sql
+++ javascript/Triangle-Judgement.sql
@@ -2,4 +2,5 @@
 -- select x,y,z,triangle
 -- from Triangle
 -- where x+y>z
+-- select *,if(x+y>z and y+z>x and z+x>y,\Yes\,\No\) as triangle from Triangle
 select *,if(x+y>z and y+z>x and z+x>y,\Yes\,\No\) as triangle from Triangle
