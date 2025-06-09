a5d7c0c373f5ceeafa82bf4ababf33d9b5d3ed63
Harishgouda G Patil <harishgouda213@gmail.com>
2025-06-09 01:52:06 +0000

    [570] [Time Beats: 96.47%] - LeetPush

--- javascript/Managers-with-at-Least-5-Direct-Reports.sql
+++ javascript/Managers-with-at-Least-5-Direct-Reports.sql
@@ -1,7 +1,13 @@
 # Write your MySQL query statement below
+-- select e.name
+--     from Employee e
+--         join Employee e1
+--             on e.id=e1.managerId
+--     group by e.id
+-- having count(e1.managerId)>=5;
 select e.name
-    from Employee e
-        join Employee e1
-            on e.id=e1.managerId
-    group by e.id
+from Employee e
+join Employee e1
+on e.id=e1.managerId
+group by e.id
 having count(e1.managerId)>=5;
