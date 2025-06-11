2ae118a496fd7f28a35a9b2a60486c73b08b26cf
Harishgouda G Patil <harishgouda213@gmail.com>
2025-06-11 02:23:12 +0000

    [1070] [Time Beats: 90.79%] - LeetPush

--- javascript/Product-Sales-Analysis-III.sql
+++ javascript/Product-Sales-Analysis-III.sql
@@ -1,5 +1,7 @@
 # Write your MySQL query statement below
-select s.product_id,s.year as first_year,s.quantity,s.price
+-- select s.product_id,s.year as first_year,s.quantity,s.price
+-- from Sales s
+-- where(s.product_id,s.year) in (select product_id,min(year) from Sales group by product_id)
+select s.product_id,s.year as first_year,s.price,s.quantity
 from Sales s
-where(s.product_id,s.year) in (select product_id,min(year) from Sales group by product_id)
-
+where (s.product_id,s.year) in (select product_id,min(year) from Sales group by product_id)
