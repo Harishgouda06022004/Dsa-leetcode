da0d4f6bd3c79eaaa6d165abf0e134948f5f2c5d
Harishgouda G Patil <harishgouda213@gmail.com>
2025-06-12 09:51:33 +0000

    [3423] [Time Beats: 23.02%] [Memory Beats: 29.16%] - LeetPush

---
+++ javascript/Maximum-Difference-Between-Adjacent-Elements-in-a-Circular-Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxAdjacentDistance(self, nums: List[int]) -> int:
+        n=len(nums)
+        ans=abs(nums[0]-nums[n-1])
+        for i in range(n):
+            ans=max(ans,abs(nums[i]-nums[i-1]))
+        return ans
+
+            
