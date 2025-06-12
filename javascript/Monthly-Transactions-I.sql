55422d1c589fdacd134ca230bffb1785af44ef11
Harishgouda G Patil <harishgouda213@gmail.com>
2025-06-12 10:26:28 +0000

    [1193] [Time Beats: 10.59%] - LeetPush

--- javascript/Monthly-Transactions-I.sql
+++ javascript/Monthly-Transactions-I.sql
@@ -1,8 +1,15 @@
 # Write your MySQL query statement below
+-- select substring(trans_date,1,7) as month,country,
+-- count(state) as trans_count,
+-- sum(state="approved") as approved_count,
+-- sum(amount) as trans_total_amount,
+-- SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+-- from Transactions
+-- group by month,country;
 select substring(trans_date,1,7) as month,country,
 count(state) as trans_count,
 sum(state="approved") as approved_count,
 sum(amount) as trans_total_amount,
-SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+sum(if(state="approved",amount,0)) as approved_total_amount
 from Transactions
-group by month,country;
+group by month,country
