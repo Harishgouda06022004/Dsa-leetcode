a9eacf582feb0148d80d3b15f3cee7438ac087a7
Harishgouda G Patil <harishgouda213@gmail.com>
2025-06-16 03:02:49 +0000

    [2016] [Time Beats: 34.22%] [Memory Beats: 89.57%] - LeetPush

--- javascript/Maximum-Difference-Between-Increasing-Elements.py
+++ javascript/Maximum-Difference-Between-Increasing-Elements.py
@@ -1,8 +1,15 @@
 class Solution:
     def maximumDifference(self, nums: List[int]) -> int:
+        min_val=nums[0]
         max_nums=-1
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i<j and nums[i]<nums[j]:
-                    max_nums=max(max_nums,nums[j]-nums[i]) 
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)):
+        #         if i<j and nums[i]<nums[j]:
+        #             max_nums=max(max_nums,nums[j]-nums[i]) 
+        # return max_nums
+        for j in range(1,len(nums)):
+            if nums[j]>min_val:
+                max_nums=max(max_nums,nums[j]-min_val)
+            else:
+                min_val=nums[j]
         return max_nums
