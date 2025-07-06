64accb0aa218fec3431dd07031eb719b59b6b725
Harishgouda G Patil <harishgouda213@gmail.com>
2025-07-06 02:07:03 +0000

    [20] [Time Beats: 8.75%] [Memory Beats: 80.53%] - LeetPush

--- javascript/Valid-Parentheses.py
+++ javascript/Valid-Parentheses.py
@@ -11,21 +11,21 @@ class Solution:
                     return False
                 top=stack[-1]
                 
-                if i==')' and top==\(\ or i==\}\ and top==\{\ or i==\]\ and top==\[\:
+                if i==')' and top=="(" or i=="}" and top=="{" or i=="]" and top=="[":
                     stack.pop()
                 else:
                     return False
         return len(stack)==0
-        stack=[]
-        for i in s:
-            if i=='(' or i=='{'or i=='[':
-                stack.append(i)
-            else:
-                if len(stack)==0:
-                    return False
-                top=stack[-1]
-                if i==')' and top=='(' or i=='}' and top=='{' or i==']' and top==']':
-                    stack.pop()
-                else:
-                    return False
-        return len(stack)==0
+        # stack=[]
+        # for i in s:
+        #     if i=='(' or i=='{'or i=='[':
+        #         stack.append(i)
+        #     else:
+        #         if len(stack)==0:
+        #             return False
+        #         top=stack[-1]
+        #         if i==')' and top=='(' or i=='}' and top=='{' or i==']' and top==']':
+        #             stack.pop()
+        #         else:
+        #             return False
+        # return len(stack)==0
