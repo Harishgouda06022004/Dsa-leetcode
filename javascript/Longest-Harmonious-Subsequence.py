3693c791c03d98a4efbe7b679f8ca5bf43a43db3
Harishgouda G Patil <harishgouda213@gmail.com>
2025-07-13 02:52:18 +0000

    [594] [Time Beats: 19.91%] [Memory Beats: 93.62%] - LeetPush

--- javascript/Longest-Harmonious-Subsequence.py
+++ javascript/Longest-Harmonious-Subsequence.py
@@ -10,9 +10,18 @@ class Solution:
         #             m=len(subseq)
         #             max_length=max(max_length,m)
         # return max_length
-        count=Counter(nums)
+        # count=Counter(nums)
+        # max_len=0
+        # for num in count:
+        #     if num+1 in count:
+        #         max_len=max(max_len,count[num]+count[num+1])
+        # return max_len
+        nums.sort()
+        left=0
         max_len=0
-        for num in count:
-            if num+1 in count:
-                max_len=max(max_len,count[num]+count[num+1])
+        for right in range(1,len(nums)):
+            while nums[right]-nums[left]>1:
+                left+=1
+            if nums[right]-nums[left]==1:
+                max_len=max(max_len,right-left+1)
         return max_len
