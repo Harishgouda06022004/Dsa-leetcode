d1730d9f27bb62bc7d8f8643e885a5b4a3d6a30e
Harishgouda G Patil <harishgouda213@gmail.com>
2025-07-14 01:47:44 +0000

    [1290] [Time Beats: 100.00%] [Memory Beats: 26.17%] - LeetPush

--- javascript/Convert-Binary-Number-in-a-Linked-List-to-Integer.py
+++ javascript/Convert-Binary-Number-in-a-Linked-List-to-Integer.py
@@ -5,9 +5,14 @@
 #         self.next = next
 class Solution:
     def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        # result=0
+        # while head:
+        #     result=result*2+head.val
+        #     head=head.next
+        # print(result)
+        # return result
         result=0
         while head:
             result=result*2+head.val
             head=head.next
-        print(result)
         return result
