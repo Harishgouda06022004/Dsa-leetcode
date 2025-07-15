61b46ffbc5498917846efe4bdac90e955017e425
Harishgouda G Patil <harishgouda213@gmail.com>
2025-07-15 04:22:46 +0000

    [3136] [Time Beats: 100.00%] [Memory Beats: 9.76%] - LeetPush

---
+++ javascript/Valid-Word.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        n=len(word)
+        if n<3:
+            return False
+        vowels=0
+        consonants=0
+        vowel_set="aeiouAEIOU"
+        for c in word:
+            if c.isalpha():
+                if c in vowel_set:
+                    vowels+=1
+                else:
+                    consonants+=1
+            elif not c.isdigit():
+                return False
+        if vowels>=1 and consonants>=1:
+            return True
+        if consonants>=1:
+            return False
+        if vowels>=1:
+            return False
