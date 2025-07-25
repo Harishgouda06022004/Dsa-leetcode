e90706104596fdccb76841be6a11ae9288f98c85
Harishgouda G Patil <harishgouda213@gmail.com>
2025-07-25 01:38:49 +0000

    [3487] [Time Beats: 3.28%] [Memory Beats: 11.24%] - LeetPush

---
+++ javascript/Maximum-Unique-Subarray-Sum-After-Deletion.py
@@ -0,0 +1,31 @@
+from collections import Counter
+class Solution:
+    def maxSum(self, nums: List[int]) -> int:
+        counter_nums=Counter(nums)
+        print(counter_nums)
+        summ=0
+        for key,value in counter_nums.items():
+            if key>0:
+                summ+=key  
+            # if len(nums)==1:
+            #     return nums[0] 
+            # if len(nums)==2 and key<0:
+            #     return max(nums)  
+            if all(k<0 for k in counter_nums.keys()):
+                return max(nums)
+        return summ
+        # left=0
+        # seen=set()
+        # n=len(nums)
+        # max_sum = float('-inf')
+        # curr_sum=0
+        # for right in range(n):
+        #     while nums[right] in seen:
+        #         seen.remove(nums[left])
+        #         curr_sum-=nums[left]
+        #         left+=1
+        #     seen.add(nums[right])
+        #     curr_sum+=nums[right]
+        #     max_sum=max(max_sum,curr_sum)
+        # return max_sum
+
