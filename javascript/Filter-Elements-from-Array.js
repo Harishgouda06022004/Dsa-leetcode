064d548f52efce0ae9ca98748cda5d6186bc9be7
Harishgouda G Patil <harishgouda213@gmail.com>
2025-09-06 03:22:08 +0000

    [2634] [Time Beats: 19.76%] [Memory Beats: 22.07%] - LeetPush

---
+++ javascript/Filter-Elements-from-Array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    // let fill=[]
+    // for (let i=0; i<arr.length; i++){
+    //     if(fn(arr[i],i)){
+    //         fill.push(arr[i])
+    //     }
+    // }
+    // return fill
+    return arr.filter(fn)
+};
