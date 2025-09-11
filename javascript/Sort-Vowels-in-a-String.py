c7d381f991c0368bd9a89ba7debfc344fc6937d5
Harishgouda G Patil <harishgouda213@gmail.com>
2025-09-11 11:59:10 +0000

    [2785] [Time Beats: 45.45%] [Memory Beats: 38.31%] - LeetPush

---
+++ javascript/Sort-Vowels-in-a-String.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        vowels=[]
+        s_list=list(s)
+        for i in s_list:
+            if i in "AEIOUaeiou":
+                vowels.append(i)
+        vowels.sort()
+        count=0
+        for j in range(len(s)):
+            if s_list[j] in "AEIOUaeiou":
+                s_list[j]=vowels[count]
+                count+=1
+        print(s_list)
+        return "".join(s_list)
+            
