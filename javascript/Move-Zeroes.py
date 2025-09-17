477c0668c28baa8d9908ca9a30e11beeb130cad6
Harishgouda G Patil <harishgouda213@gmail.com>
2025-09-17 13:54:12 +0000

    [283] [Time Beats: 41.72%] [Memory Beats: 49.46%] - LeetPush

--- javascript/Move-Zeroes.py
+++ javascript/Move-Zeroes.py
@@ -2,9 +2,10 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         n=len(nums)
         j=0
-        for i in range(0,n):
+        for  i in range(0,n):
             if nums[i]!=0:
                 nums[i],nums[j]=nums[j],nums[i]
                 j+=1
         return nums
+
         
