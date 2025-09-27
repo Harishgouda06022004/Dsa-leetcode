1bcd3a59fafeb143373770fab4ccc493496502ea
Harishgouda G Patil <harishgouda213@gmail.com>
2025-09-27 12:54:53 +0000

    [812] [Time Beats: 12.05%] [Memory Beats: 30.76%] - LeetPush

---
+++ javascript/Largest-Triangle-Area.py
@@ -0,0 +1,13 @@
+class Solution:
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        n=len(points)
+        area=0.1
+        for i in range(n-2):
+            x1,y1=points[i]
+            for j in range(n-1):
+                x2,y2=points[j]
+                for k in range(n):
+                    x3,y3=points[k]
+                    if abs(0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)))>area:
+                        area=abs(0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)))
+        return area
