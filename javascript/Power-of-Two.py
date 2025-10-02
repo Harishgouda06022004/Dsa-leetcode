331e603a2afb57e806cb1f728d0370d2b6d53ed9
Harishgouda G Patil <harishgouda213@gmail.com>
2025-10-02 10:21:44 +0000

    [231] [Time Beats: 4.81%] [Memory Beats: 14.23%] - LeetPush

--- javascript/Power-of-Two.py
+++ javascript/Power-of-Two.py
@@ -1,22 +1,9 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        # while n>0:
-        #     if n%2==2:
-        #         return False
-        #     n//=2
-        # return True
-        # return n>0 and (n&(n-1))==0
-        # if n<=0:
-        #     return False
-        # while n>1:
-        #     if n%2!=0:
-        #         return False
-        #     n//=2
-        # return True
         if n<=0:
             return False
         while n>1:
             if n%2!=0:
                 return False
-            n=n//2
+            n//=2
         return True
