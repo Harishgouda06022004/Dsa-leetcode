06d614070204f139283eb0f7901c01ecc0505e25
Harishgouda G Patil <harishgouda213@gmail.com>
2025-10-09 07:37:22 +0000

    [2704] [Time Beats: 16.61%] [Memory Beats: 12.96%] - LeetPush

--- javascript/To-Be-Or-Not-To-Be.js
+++ javascript/To-Be-Or-Not-To-Be.js
@@ -4,24 +4,23 @@
  */
 var expect = function(val) {
     return{
-        toBe:function(otherVal){
-            if (val===otherVal){
-                return true
-            }
-            else{
-                throw new Error("Not Equal");
-            }
-        },
-        notToBe:function(otherVal){
-            if (val!==otherVal){
-                return true
-            }
-            else{
-                throw new Error("Equal");
-            }
+       toBe:function(otherVal){
+        if(val===otherVal){
+            return true
+        }else{
+            throw new Error("Not Equal")
         }
+       },
+       notToBe:function(otherVal){
+        if(val!==otherVal){
+            return true
+        }else{
+            throw new Error("Equal")
+        }
+       }
     }
-};
+
+    }
 
 /**
  * expect(5).toBe(5); // true
