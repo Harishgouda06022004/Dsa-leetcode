0fc3065ea38eb88bb0a781e94c9cb2f88d9ae915
Harishgouda G Patil <harishgouda213@gmail.com>
2025-10-10 08:09:42 +0000

    [35] [Time Beats: 100.00%] [Memory Beats: 26.65%] - LeetPush

--- javascript/Search-Insert-Position.py
+++ javascript/Search-Insert-Position.py
@@ -1,15 +1,11 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        low=0
-        n=len(nums)
-        high=n-1
-        ans=0
-        while low<=high:
-            mid=(low+high)//2
-            if nums[mid]>=target:
-                ans=mid
-                high=mid-1
+        left = 0
+        rigth=len(nums)
+        while left<rigth:
+            mid=(left+rigth)//2
+            if nums[mid]<target:
+                left+=1
             else:
-                low=mid+1
-        ans=low
-        return ans
+                rigth=mid
+        return left
