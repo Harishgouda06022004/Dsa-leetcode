218592b20470c1b7b9257d0fc96c350f6a2dfd2f
Harishgouda G Patil <harishgouda213@gmail.com>
2025-10-22 07:41:52 +0000

    [796] [Time Beats: 100.00%] [Memory Beats: 48.78%] - LeetPush

---
+++ javascript/Rotate-String.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s)!=len(goal):
+            return False
+        t=s+s
+        if goal in t:
+            return True
+        return False
