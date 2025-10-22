9789a5c533e87bfc6dec0c6d755bda76b9d8ca36
Harishgouda G Patil <harishgouda213@gmail.com>
2025-10-22 07:32:33 +0000

    [2625] [Time Beats: 74.26%] [Memory Beats: 92.56%] - LeetPush

--- javascript/Flatten-Deeply-Nested-Array.js
+++ javascript/Flatten-Deeply-Nested-Array.js
@@ -4,5 +4,18 @@
  * @return {Array}
  */
 var flat = function (arr, n) {
+    // if(n==0){
+    //     return arr
+    // }
+    // let result=[]
+    // for(let el of arr){
+    //     if(Array.isArray(el)){
+    //         result.push(...flat(el,n-1));
+    //     }else{
+    //         result.push(el)
+    //     }
+
+    // } 
+    // return result
     return arr.flat(n)
 };
