6213e4ea8346e1ab0e1f6a96e86ac9533997c398
Harishgouda G Patil <harishgouda213@gmail.com>
2026-01-23 16:22:05 +0000

    [1716] [Time Beats: 45.20%] [Memory Beats: 13.95%] - LeetPush

--- javascript/Calculate-Money-in-Leetcode-Bank.py
+++ javascript/Calculate-Money-in-Leetcode-Bank.py
@@ -1,11 +1,12 @@
-class Solution:
-    def totalMoney(self, n: int) -> int:
-        total=0
-        week_start=1
-        for i in range(1,n+1):
-            day_of_week=(i-1)%7
-            total+=week_start+day_of_week
-            if day_of_week==6:
-                week_start+=1
-        return total
-            
+1class Solution:
+2    def totalMoney(self, n: int) -> int:
+3        total=0
+4        week_start=1
+5        for i in range(1,n+1):
+6            day_of_week=(i-1)%7
+7            total+=week_start+day_of_week
+8            if day_of_week==6:
+9                week_start+=1
+10        return total
+11            
+12
