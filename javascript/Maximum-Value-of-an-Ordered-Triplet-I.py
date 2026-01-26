fb7dfe0fd3fcb7f4c2949d304c2248f61155e614
Harishgouda G Patil <harishgouda213@gmail.com>
2026-01-26 08:12:33 +0000

    [2873] [Time Beats: 43.23%] [Memory Beats: 26.20%] - LeetPush

--- javascript/Maximum-Value-of-an-Ordered-Triplet-I.py
+++ javascript/Maximum-Value-of-an-Ordered-Triplet-I.py
@@ -1,11 +1,11 @@
-class Solution:
-    def maximumTripletValue(self, nums: List[int]) -> int:
-        max_value=0
-        n=len(nums)
-        for i in range(n):
-            for j in range(i+1,n):
-                for k in range(j+1,n):
-                    current_value=(nums[i] - nums[j]) * nums[k]
-                    if current_value>max_value:
-                       max_value=current_value
-        return max_value if max_value>0 else 0
+1class Solution:
+2    def maximumTripletValue(self, nums: List[int]) -> int:
+3        max_value=0
+4        n=len(nums)
+5        for i in range(n):
+6            for j in range(i+1,n):
+7                for k in range(j+1,n):
+8                    current_value=(nums[i] - nums[j]) * nums[k]
+9                    if current_value>max_value:
+10                       max_value=current_value
+11        return max_value if max_value>0 else 0
