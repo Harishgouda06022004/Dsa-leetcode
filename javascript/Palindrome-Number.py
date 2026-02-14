58ee28fc0abbd138e856be411b032df3e1d6433b
Harishgouda G Patil <harishgouda213@gmail.com>
2026-02-14 13:40:05 +0530

    [9] [Time Beats: 63.86%] [Memory Beats: 38.32%] - LeetPush

--- javascript/Palindrome-Number.py
+++ javascript/Palindrome-Number.py
@@ -1,18 +1,12 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        # if x<0:
-        #     return False
-        # return str(x)==str(x)[::-1]
-        if x<0:
-            return False
-        rev=0
-        orginal_x=x
-        # sign=-1 if x<0 else +1
-        while x>0:
-            digit=x%10
-            rev=rev*10+digit
-            x=x//10
-        return orginal_x==rev 
-
-
-            
+1class Solution:
+2    def isPalindrome(self, x: int) -> bool:
+3        if x<0:
+4            return False
+5        rev=0
+6        orginal_x=x
+7        # sign=-1 if x<0 else +1
+8        while x>0:
+9            digit=x%10
+10            rev=rev*10+digit
+11            x=x//10
+12        return orginal_x==rev 
