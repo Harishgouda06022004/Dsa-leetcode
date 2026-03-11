2e4b13e60ec669ef262546b2ee64c7c1bc4f2f25
Harishgouda G Patil <harishgouda213@gmail.com>
2026-03-11 10:18:11 +0530

    [1009] [Time Beats: 100.00%] [Memory Beats: 58.78%] - LeetPush

--- javascript/Complement-of-Base-10-Integer.py
+++ javascript/Complement-of-Base-10-Integer.py
@@ -1,5 +1,5 @@
-class Solution:
-    def bitwiseComplement(self, n: int) -> int:
-        bin_s=bin(n)[2:]
-        flipped=''.join('1' if b=='0' else '0' for b in bin_s)
-        return int(flipped,2)
+1class Solution:
+2    def bitwiseComplement(self, n: int) -> int:
+3        bin_s=bin(n)[2:]
+4        flipped=''.join('1' if b=='0' else '0' for b in bin_s)
+5        return int(flipped,2)
