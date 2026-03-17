2a5ef4f5f6156c10ceb5aa7fc6b1dcbe8b63d0e4
Harishgouda G Patil <harishgouda213@gmail.com>
2026-03-17 08:35:46 +0530

    [2843] [Time Beats: 75.19%] [Memory Beats: 95.43%] - LeetPush

--- javascript/Count-Symmetric-Integers.py
+++ javascript/Count-Symmetric-Integers.py
@@ -1,19 +1,20 @@
-class Solution:
-    def countSymmetricIntegers(self, low: int, high: int) -> int:
-        count=0
-        for x in range(low,high+1):
-            str_x=str(x)
-            length=len(str_x)
-            if length%2!=0:
-                continue
-            n=length/2
-            sum1=0
-            sum2=0
-            n=int(n)
-            for i in range(0,n):
-                sum1+=int(str_x[i])
-            for j in range(n,length):
-                sum2+=int(str_x[j])
-            if sum1==sum2:
-                count+=1
-        return count
+1class Solution:
+2    def countSymmetricIntegers(self, low: int, high: int) -> int:
+3        count=0
+4        for x in range(low,high+1):
+5            str_x=str(x)
+6            length=len(str_x)
+7            if length%2!=0:
+8                continue
+9            n=length/2
+10            sum1=0
+11            sum2=0
+12            n=int(n)
+13            for i in range(0,n):
+14                sum1+=int(str_x[i])
+15            for j in range(n,length):
+16                sum2+=int(str_x[j])
+17            if sum1==sum2:
+18                count+=1
+19        return count
+20
