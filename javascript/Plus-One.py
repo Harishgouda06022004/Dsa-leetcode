a889a75687adfa80431f1b68c4476198496a0dea
Harishgouda G Patil <harishgouda213@gmail.com>
2026-04-03 08:15:29 +0530

    [66] [Time Beats: 100.00%] [Memory Beats: 93.46%] - LeetPush

--- javascript/Plus-One.py
+++ javascript/Plus-One.py
@@ -1,12 +1,14 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        num=int(''.join(map(str,digits)))
-        print(num)
-        num=num+1
-        print(num)
-        num=str(num)
-        print(num)
-        num=list(map(int ,str(num)))
-        print(num)
-        return num
-        
+1class Solution:
+2    def plusOne(self, digits: List[int]) -> List[int]:
+3        s = ""
+4        for num in digits:
+5            s += str(num)
+6
+7        s = int(s) + 1
+8        s = str(s)
+9
+10        arr = []
+11        for i in s:
+12            arr.append(int(i))  
+13        return arr
+14
