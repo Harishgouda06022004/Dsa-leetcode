ee71b9b51bee7e0ddc2548b20b36aa4c6d74623b
Harishgouda G Patil <harishgouda213@gmail.com>
2026-05-22 09:47:23 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 77.83%] - LeetPush

--- javascript/Search-in-Rotated-Sorted-Array.py
+++ javascript/Search-in-Rotated-Sorted-Array.py
@@ -1,38 +1,6 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        # nums=sorted(nums)
-        # print(nums)
-        n=len(nums)
-        # left=0
-        # right=n-1
-        # while left<right:
-        #     mid=(left+right)//2
-        #     if nums[mid]==target:
-        #         return nums[mid]
-        #     elif nums[mid]<target:
-        #         left=mid+1
-        #     else:
-        #         right=mid-1
-        # return -1
-        # for num in range(0,n):
-        #     if nums[num]==target:
-        #         return num
-        # return -1
-        left=0
-        right=n-1
-        while left<=right:
-            mid=(left+right)//2
-            if nums[mid]==target:
-                return mid
-            if nums[left]<=nums[mid]:
-                if nums[left]<=target and target<=nums[mid]:
-                    right=mid-1
-                else:
-                    left=mid+1
-            else:
-                if nums[mid]<=target and target<=nums[right]:
-                    left=mid+1
-                else:
-                    right=mid-1
-        return -1
-
+1class Solution:
+2    def search(self, nums: List[int], target: int) -> int:
+3        for i in range(0,len(nums)):
+4            if nums[i]==target:
+5                return i
+6        return -1
