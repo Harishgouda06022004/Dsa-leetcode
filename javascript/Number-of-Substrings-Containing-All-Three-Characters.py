dfbdcbee3e7d0676878ae990bace1ee740c21990
Harishgouda G Patil <harishgouda213@gmail.com>
2026-06-30 09:46:22 +0530

    [1358] [Time Beats: 80.13%] [Memory Beats: 48.97%] - LeetPush

--- javascript/Number-of-Substrings-Containing-All-Three-Characters.py
+++ javascript/Number-of-Substrings-Containing-All-Three-Characters.py
@@ -1,20 +1,10 @@
-from collections import defaultdict
-class Solution:
-    def numberOfSubstrings(self, s: str) -> int:
-        n=len(s)
-        print(n)
-        char_count=defaultdict(int)
-        l=0
-        count=0
-        for r in range(n):
-            char_count[s[r]]+=1
-            while len(char_count)==3:
-                count+=n-r
-                char_count[s[l]]-=1
-                if char_count[s[l]]==0:
-                    del char_count[s[l]]
-                l+=1
-        return count
-    
-
-            
+1class Solution:
+2    def numberOfSubstrings(self, s: str) -> int:
+3        last_seen={'a':-1,'b':-1,'c':-1}
+4        count=0
+5        for i, char, in enumerate(s):
+6            last_seen[char]=i
+7            if last_seen['a']!=-1 and last_seen['b']!=-1 and last_seen['c']!=-1:
+8                min_idx=min(last_seen['a'],last_seen['b'],last_seen['c'])
+9                count+=min_idx+1
+10        return count
