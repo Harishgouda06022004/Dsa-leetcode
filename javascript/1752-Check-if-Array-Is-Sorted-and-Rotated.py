4a2ae36d7edc3d3d9efafe0325f836bdaf021a46
Harishgouda G Patil <harishgouda213@gmail.com>
2026-07-09 14:27:16 +0530

    [1752] [Time Beats: 100.00%] [Memory Beats: 52.80%] - LeetPush

--- javascript/1752-Check-if-Array-Is-Sorted-and-Rotated.py
+++ javascript/1752-Check-if-Array-Is-Sorted-and-Rotated.py
@@ -1,8 +1,7 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
         s=sorted(nums)
-        n=len(nums)
-        for i in range(n):
+        for i in range(len(nums)):
             rotated=nums[i:]+nums[:i]
             if s==rotated:
                 return True
