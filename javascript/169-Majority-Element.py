2f898668e5906c20697c97d41f69c5dc25378d87
Harishgouda G Patil <harishgouda213@gmail.com>
2026-08-09 15:47:44 +0530

    [169] [Time Beats: 32.93%] [Memory Beats: 18.69%] - LeetPush

--- javascript/169-Majority-Element.py
+++ javascript/169-Majority-Element.py
@@ -3,8 +3,9 @@ class Solution:
         dict1={}
         for num in nums:
             if num in dict1:
-                dict1[num]=dict1.get(num,0)+1
+                dict1[num]+=1
             else:
                 dict1[num]=1
-        print(dict1)
         return max(dict1,key=dict1.get)
+
+
