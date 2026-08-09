1479f255e1e24c9b0fdda403e5bb7909894f9d64
Harishgouda G Patil <harishgouda213@gmail.com>
2026-08-09 16:00:49 +0530

    [121] [Time Beats: -%] [Memory Beats: -%] - LeetPush

--- javascript/121-Best-Time-to-Buy-and-Sell-Stock.py
+++ javascript/121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,20 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # n=len(prices)
-        # # low=min(arr)
-        # # high=max(arr)
-        # max_profit=0
-        # for i in range(0,n):
-        #     for j in range(i+1,n):
-        #         profit = prices[j] - prices[i]  # Calculate profit
-        #         max_profit = max(max_profit, profit)
-        # return max_profit
-        min_price=float('inf')
         max_profit=0
-        for price in prices:
-            if price<min_price:
-                min_price=price
-            else:
-                max_profit=max(max_profit,price-min_price)
+        min_price=float('inf')
+        for i in range(len(prices)):
+            if prices[i]<min_price:
+                min_price=prices[i]
+            profit=prices[i]-min_price
+            if profit>0:
+                max_profit=max(max_profit,profit)
         return max_profit
-                
