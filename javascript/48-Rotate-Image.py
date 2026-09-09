682768597cfb717a894aedca533770f376853275
Harishgouda G Patil <harishgouda213@gmail.com>
2026-09-09 21:38:58 +0530

    [48] [Time Beats: 5.65%] [Memory Beats: 68.81%] - LeetPush

--- javascript/48-Rotate-Image.py
+++ javascript/48-Rotate-Image.py
@@ -1,12 +1,11 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        # result=[list(col)[::-1] for col in zip(*matrix)]
-        # print(result)
-        # return result
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
         n=len(matrix)
         for i in range(n):
             for j in range(i,n):
                 matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
         for row in matrix:
             row.reverse()
-        return matrix
